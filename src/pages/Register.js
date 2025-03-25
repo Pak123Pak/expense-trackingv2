ef3139e474cfc642be3b00d896dc0b1175511409
Pak123Pak <savefile6709u@gmail.com>
2025-03-25 07:21:00 +0100

    error 2

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -25,7 +25,7 @@ export default function Register() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        console.log("Version 2");
+        console.log("Version 3");
     }, []);
 
     async function handleSubmit(e) {
